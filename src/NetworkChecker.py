e9f1b5277ec48d8224102f2bdc18384c329bcd2a
Sebastian van Bergerem <sebastian.v.bergerem@gmail.com>
2026-05-30 20:17:01 +0200

    Checkpoint

---
+++ src/NetworkChecker.py
@@ -0,0 +1,70 @@
+import platform
+import subprocess
+import os
+import re
+from ping3 import ping
+
+class NetworkChecker:
+    
+    def __init__(self):
+        pass
+    
+    def test_latency(self, ip_address):
+        result = {
+            "target": ip_address,
+            "success": None,
+            "latency_ms": None,
+            "error_message": None
+        }
+          
+        try:
+            system = platform.system()
+            
+            ping_result = None  
+            
+            if system == "Windows":
+                latency = ping(ip_address, unit="ms")
+                
+                if isinstance(latency, (int, float)) and not isinstance(latency, bool):
+                    result["success"] = True
+                    result["latency_ms"] = latency
+                else:
+                    result["success"] = False
+                                    
+            elif system in ["Linux", "Darwin"]:
+                env = os.environ.copy()
+                env["LC_ALL"] = "C"
+                env["LANG"] = "C"
+                ping_result = subprocess.run(
+                    ["ping", "-c", "1",  ip_address],
+                    capture_output=True,
+                    text=True,
+                    timeout=5,
+                    encoding="utf-8",
+                    errors="replace",
+                    env=env
+                )
+                
+                if ping_result.returncode == 0:
+                    latency_text = re.search(r"time=\s*([0-9]+(?:\.[0-9]+)?)\s*ms", ping_result.stdout)
+                    
+                    result["success"] = True
+                    
+                    if latency_text is not None:
+                        result["latency_ms"] = float(latency_text.group(1))
+                    else:
+                        result["latency_ms"] = None
+                        result["error_message"] = ping_result.stderr
+                else:
+                    result["success"] = False
+                    
+            else:
+                result["success"] = False
+                result["error_message"] = "Error: Can't ping. OS unknown"
+    
+        except Exception as ex:
+            result["success"] = False
+            result["error_message"] = ex
+            
+        return result
+            
--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("InterConMon started")
+from NetworkChecker import NetworkChecker 
+
+
+if __name__ == "__main__":
+    nc = NetworkChecker()
+    print(nc.test_latency("1.1.1.1"))
+    print(nc.test_latency("8.8.8.8"))
+    print(nc.test_latency("111.1111.1.1"))
+    
